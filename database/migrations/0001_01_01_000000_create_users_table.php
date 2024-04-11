1d7882116b24860b7069956dd48bc290951ed2b1
GerardTums <gerardo.tumlos012@gmail.com>
2024-04-12 05:13:33 +0800

    CRUD dental api

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -107,6 +107,13 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('membership');
+        Schema::dropIfExists('procedures');
+        Schema::dropIfExists('rescedules');
+        Schema::dropIfExists('apointments');
+        Schema::dropIfExists('promos');
+        Schema::dropIfExists('nurses');
+        Schema::dropIfExists('dentists');
+        Schema::dropIfExists('profiles');
         Schema::dropIfExists('users');
     }
 };
